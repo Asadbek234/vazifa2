97a09559a54e5c9f266c0501d8f038e6637b6a07
Asadbek234 <sobirovasadbek094@gmail.com>
2024-07-18 21:43:42 +0500

    Update postgres.sql
    tugadi

--- postgres.sql
+++ postgres.sql
@@ -1,1 +1,15 @@
-
+create table if not exists user(
+  id serial primary key,
+  first_name varchar(255) not null,
+  last_name varchar(255) not null,
+  age int not null
+  
+);
+create table if not exists product(
+  id serial primary key,
+  product_name varchar(255), 
+  price numberic(5,2 ),
+  user_id serial,
+  foreign key (user_id) references user(id)
+  
+);
